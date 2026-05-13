0def5486bb6dff63dc2a05e2b6799ddf2e063af5
panasafaps68-rgb <panas.afaps68@gmail.com>
2026-05-13 20:08:57 +0700

    Remove unused imports from news_bot.py
    Removed unused imports from the news_bot.py file.

--- news_bot.py
+++ news_bot.py
@@ -1,6 +1,4 @@
 import os
-import requests
-from openai import OpenAI
 
 OPENAI_API_KEY = os.environ["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"]
 TELEGRAM_BOT_TOKEN = os.environ["8403970315:AAHN-OVWvvsCsWghIIO4yos-txyIIMniEW8"]
